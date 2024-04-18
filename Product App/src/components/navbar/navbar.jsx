b02ce5d927791c5bfc88980647705dc376cc65ce
Alexandru Avram <alexandruavram46@gmail.com>
2024-04-18 20:38:53 +0300

    commit1

---
+++ Product App/src/components/navbar/navbar.jsx
@@ -0,0 +1,11 @@
+import './navbar.css'
+
+const Navbar = () => {
+    return (
+        <div className='nav-bar'>
+            <h1 className='nav-title'>Aprozar gen</h1>
+        </div>
+    );
+};
+
+export default Navbar;
